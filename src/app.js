1f3a822ef1f1d3131a95edcc7b018bd6c7766136
Arsh Gogia <arshgogia20@gmail.com>
2025-03-10 16:53:53 +0400

    user schema & model

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  // Creating a instance of the User model
+  const user = new User({
+    firstName: "Arsh",
+    lastName: "Gogia",
+    emailId: "arshgogia20@gmail.com",
+    password: "arsh1620",
+    age: 20,
+    gender: "Male",
+    _id: "18889088467382946",
+  });
+
+  try {
+    await user.save(); //returns a promise
+    res.send("User added succesfully");
+  } catch (err) {
+    res.status(400).send("Error adding user: " + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
