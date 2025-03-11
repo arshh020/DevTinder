8ede886f34620639affab7c96abc02daa10b3f30
Arsh Gogia <arshgogia20@gmail.com>
2025-03-11 23:58:40 +0400

    user APIs for db

--- src/app.js
+++ src/app.js
@@ -3,23 +3,79 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
   // Creating a instance of the User model
-  const user = new User({
-    firstName: "Arsh",
-    lastName: "Gogia",
-    emailId: "arshgogia20@gmail.com",
-    password: "arsh1620",
-    age: 20,
-    gender: "Male",
-    _id: "18889088467382946",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save(); //returns a promise
     res.send("User added succesfully");
   } catch (err) {
     res.status(400).send("Error adding user: " + err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong : " + err.message);
+  }
+});
+
+app.get("/userId", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findById(userId);
+    if (user.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong : " + err.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    await User.findByIdAndUpdate(userId, data);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong : " + err.message);
   }
 });
 
